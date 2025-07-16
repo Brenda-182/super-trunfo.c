9a765e69ed8ecbc7ee21031c20ffe3e054143b13
Brenda-182 <brendaassis2002@gmail.com>
2025-07-16 15:00:53 -0300

    Tudo feito

--- super-trunfo.c
+++ super-trunfo.c
@@ -1,22 +1,42 @@
-#include <stdio.h>
 int main(){
 
-    //Carta 1 variáveis
+    //Atribuindo valores de variáveis
+    //Carta 1
     char estado = 'A';
     char codigo[4] = "A01";
     char nomeCidade[20] = "São Paulo";
     int populacao = 12325000
     float area = 1521.11
-    float pib = 699,28
+    float pib = 699.28
     int pontosTuristicos = 50
 
-    //Acrescentando strings
-    printf("Estdo: %c\n", estado);
-    printf("Código: %c\n", codigo);
-    printf("Nome da Cidade: %s\n", nomeCidade);
-    printf("População: %d\n", populacao);
-    printf("Área: %f.2 Km²\n", area);
-    printf("Pib: %f.2 bilhões de reais\n", pib);
-    printf("Pontos Túristicos: %d\n", pontosTuristicos);
-    return 0;
+    //Atribuindo valores as váriaveis
+    //Carta 2
+    char estado2 = 'B';
+    char codigo2[4] = "B02";
+    char nomeCidade2[20] = "Rio de Janeiro";
+    int populacao2 = 6748000
+    float area2 = 1200.25
+    float pib2 = 300.50
+    int pontosTuristicos2 = 30
+
+    //Acrescentando as strings Carta1
+    print("Carta 1:\n");
+    printf("Estdo: %c\n", estado1);
+    printf("Código: %c\n", codigo1);
+    printf("Nome da Cidade: %s\n", nomeCidade1);
+    printf("População: %d\n", populacao1);
+    printf("Área: %f.2 Km²\n", area1);
+    printf("Pib: %f.2 bilhões de reais\n", pib1);
+    printf("Pontos Túristicos: %d\n", pontosTuristicos1);
+    
+     //Acrescentando as strings Carta2
+     printf("Carta 2:\n");
+    printf("Estdo: %c\n", estado2);
+    printf("Código: %c\n", codigo2);
+    printf("Nome da Cidade: %s\n", nomeCidade2);
+    printf("População: %d\n", populacao2);
+    printf("Área: %f.2 Km²\n", area2);
+    printf("Pib: %f.2 bilhões de reais\n", pib2);
+    printf("Pontos Túristicos: %d\n", pontosTuristicos2);
 }
